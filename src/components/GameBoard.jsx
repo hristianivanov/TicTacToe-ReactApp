06ebaef2acaa74826d7981d16552cb58496cb69c
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2025-05-17 11:37:30 +0300

    handle selecting square with X

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,13 +7,22 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArr => [...innerArr])];
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            return updatedGameBoard;
+        })
+    }
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}><button></button></li>
+                            <li key={colIndex}><button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>
                         ))}
                     </ol>
                 </li>
