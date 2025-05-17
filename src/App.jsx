ecd6bb140729f706194c8699c50ee1d533b0842d
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2025-05-17 09:30:25 +0300

    separate component to simplify the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "../components/Player"
 
 function App() {
 
@@ -6,16 +7,10 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
-      GAME BOARD
+        GAME BOARD
       </div>
       LOG
     </main>
